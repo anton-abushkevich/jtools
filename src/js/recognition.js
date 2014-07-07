55508a3e9b269280f6f2de4be230c3d81a529a8c
Anton Abushkevich <anton.abushkevich@gmail.com>
2014-07-07 20:09:19 +0400

    parse stroke data

--- src/js/recognition.js
+++ src/js/recognition.js
@@ -31,16 +31,23 @@ function Recognition() {
     }
 
     function doInit(recog) {
+        var start = new Date;
+        console.log("Parse start: " + start);
         for (var k in recog) {
-            var kanji = new Kanji(k, recog[k]);
+            if (!recog.hasOwnProperty(k)) {
+                continue;
+            }
 
-            var sub = kanjis[kanji.strokes.length];
+            var kanji = new Kanji(k, recog[k]),
+                sub = kanjis[kanji.strokes.length];
+
             if (!sub) {
                 sub = {};
                 kanjis[kanji.strokes.length] = sub;
             }
             sub[kanji.symbol] = kanji;
         }
+        console.log("Parse complete in: " + ((new Date - start) / 1000) + " sec");
     }
 
     function Kanji(symbol, strokesRecogData) {
@@ -52,9 +59,20 @@ function Recognition() {
         }
 
         function parseRecogData(strokesRecogData, strokes) {
-            var rnd = Math.floor(Math.random() * 10 + 1);
-            for (var i = 0; i < rnd; i++) {
-                strokes[i] = i + rnd;
+            strokesRecogData = strokesRecogData.replace(/x/g, "00").replace(/y/g, "ff");
+            if (strokesRecogData.length % 8 != 0) {
+                //console.log("Wrong data for " + symbol + ": \"" + strokesRecogData + "\"");
+                return;
+            }
+            var count = strokesRecogData.length / 8;
+            for (var i = 0; i < count; i++) {
+                var stroke = strokesRecogData.substr(i * 8, 8);
+                strokes[i] = [
+                    parseInt(stroke.substr(0, 2), 16),
+                    parseInt(stroke.substr(2, 2), 16),
+                    parseInt(stroke.substr(4, 2), 16),
+                    parseInt(stroke.substr(6, 2), 16)
+                ];
             }
         }
     }
